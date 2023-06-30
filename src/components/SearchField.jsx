4237ea3868e8aef33ed2e93c56c018aeaecf68c7
Vladislav <vladdevs@yandex.ru>
2023-06-30 08:07:30 +0300

    Refactoring

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -3,7 +3,7 @@ function SearchField({ handleSearch, handleClear, handleChange, searchQuery }) {
     <div className='search'>
       <form onSubmit={handleSearch} className='search__form' autocomplete='off'>
         <input type='text' autoFocus autocomplete='nope' value={searchQuery} onChange={handleChange} placeholder='Найдутся все гифки' className='search__field' />
-        <button type='reset' onClick={handleClear} className='search__button search__button_clear'></button>
+        {searchQuery && <button type='reset' onClick={handleClear} className='search__button search__button_clear'></button>}
         <button type='submit' className='search__button search__button_search'></button>
       </form>
     </div>
